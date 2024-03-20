dc3a768d14e833d81a8193944b6a7c7cf902ea36
Mehdi Benamar <mistahbe213@gmail.com>
2024-03-20 16:11:04 +0100

    get challenges from firebas and display the state of challenges

--- src/components/ChallengeCard.jsx
+++ src/components/ChallengeCard.jsx
@@ -1,25 +1,24 @@
 import React from "react"
 
-function ChallengeCard() {
+function ChallengeCard({ challenge }) {
   return (
     <>
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
           <img
-            class="rounded-t-lg"
+            className="rounded-t-lg"
             src="https://s3-eu-west-1.amazonaws.com/sdz-upload/prod/upload/logo_officiel.png"
             alt=""
           />
         </a>
-        <div class="p-5">
+        <div className="p-5">
           <a href="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
+              {challenge.titre}
             </h5>
           </a>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+            {challenge.description}
           </p>
         </div>
       </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,31 @@
-import React from "react"
+import React, { useDebugValue, useEffect } from "react"
 import Header from "../components/Header"
 import ChallengeCard from "../components/ChallengeCard"
+import { getChallenges } from "../components/firebase"
+import { useSelector, useDispatch } from "react-redux"
+import { setChallenges } from "../store/challenge/challengeSlice"
 
 function Dashboard() {
+  const { challenges } = useSelector((state) => state.challenge)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const getChallengesApi = async () => {
+      const data = await getChallenges()
+      dispatch(setChallenges(data))
+    }
+
+    getChallengesApi()
+  }, [])
+
   return (
     <>
       <Header />
       <div>Barre de progression des challenges réussis</div>
       <main className="flex justify-center items-start gap-5 flex-wrap my-8">
-        <ChallengeCard />
+        {challenges.map((challenge) => {
+          return <ChallengeCard key={challenge.id} challenge={challenge} />
+        })}
       </main>
     </>
   )
