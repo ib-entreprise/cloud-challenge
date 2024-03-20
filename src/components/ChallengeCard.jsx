537d76fdb7aecd3db812d269e8b0ea237777e878
Mehdi Benamar <mistahbe213@gmail.com>
2024-03-20 14:32:05 +0100

    affichage des challenges sur dashboard

--- src/components/ChallengeCard.jsx
+++ src/components/ChallengeCard.jsx
@@ -1,7 +1,30 @@
 import React from "react"
 
 function ChallengeCard() {
-  return <div>ChallengeCard</div>
+  return (
+    <>
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img
+            class="rounded-t-lg"
+            src="https://s3-eu-west-1.amazonaws.com/sdz-upload/prod/upload/logo_officiel.png"
+            alt=""
+          />
+        </a>
+        <div class="p-5">
+          <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default ChallengeCard
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,15 @@
 import React from "react"
 import Header from "../components/Header"
+import ChallengeCard from "../components/ChallengeCard"
 
 function Dashboard() {
   return (
     <>
       <Header />
+      <div>Barre de progression des challenges réussis</div>
+      <main className="flex justify-center items-start gap-5 flex-wrap my-8">
+        <ChallengeCard />
+      </main>
     </>
   )
 }
