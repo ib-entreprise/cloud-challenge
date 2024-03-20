3af05d5bca3e9e21d51e2842c23b7a28f598f7b3
Mehdi Benamar <mistahbe213@gmail.com>
2024-03-20 16:31:27 +0100

    create user store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 import ChallengeSlice from "./challenge/challengeSlice"
 import SolutionSlice from "./solution/solutionSlice"
+import UserSlice from "./user/userSlice"
 
 export const store = configureStore({
   reducer: {
     challenge: ChallengeSlice.reducer,
     solution: SolutionSlice.reducer,
+    user: UserSlice.reducer,
   },
 })
---
+++ src/store/user/userSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const UserSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: {},
+  },
+  reducers: {},
+})
+
+export default UserSlice
