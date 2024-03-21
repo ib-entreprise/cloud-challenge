ad2231814b3f340f89c2af6836d580672c2b1595
Mehdi Benamar <mistahbe213@gmail.com>
2024-03-21 16:56:49 +0100

    feature/connexion du user

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,60 +1,73 @@
 import { React, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "./firebase"
+import { toast } from "react-toastify"
 
 function FormLogin() {
+  const navigate = useNavigate()
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, password)
+      toast.success("Vous êtes connecté")
+      navigate("/dashboard")
+      console.log(user)
+    } catch (error) {
+      toast.error("Email et/ou mot de passe incorrect !")
+    }
+  }
   return (
-    <section>
-      <form class="max-w-sm mx-auto" method="post" onSubmit={handleLogin}>
-        <div class="mb-5">
-          <label
-            for="email"
-            class="block mb-2 text-sm font-medium text-gray-900"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            onChange={handleEmailLoginChange}
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@flowbite.com"
-            required
-          />
-        </div>
-        <div class="mb-5">
-          <label
-            for="password"
-            class="block mb-2 text-sm font-medium text-gray-900"
-          >
-            Your password
-          </label>
-          <input
-            type="password"
-            id="password"
-            onChange={handlePasswordLoginChange}
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
-          />
-        </div>
-        <div class="flex items-start mb-5">
-          <div class="flex items-center h-5">
+    <>
+      <div>
+        <form onSubmit={handleLogin} className="max-w-sm mx-auto my-10">
+          <div className="max-w-sm mx-auto my-10">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Votre email
+            </label>
             <input
-              id="remember"
-              type="checkbox"
-              value=""
-              class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-              required
+              type="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              required={email !== ""}
             />
           </div>
-          <label
-            for="remember"
-            class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Mot de passe
+            </label>
+            <input
+              id="password"
+              type="password"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Mot de passe"
+              required={password !== ""}
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full text-black bg-blue-500 hover:bg-blue-700 hover:text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           >
-            Remember me
-          </label>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </section>
+            Connexion
+          </button>
+        </form>
+      </div>
+    </>
   )
 }
 
