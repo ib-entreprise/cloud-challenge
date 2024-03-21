472687bd3e9882d2d0abab3b19d537d0258a1bf6
Mehdi Benamar <mistahbe213@gmail.com>
2024-03-21 15:03:34 +0100

    création de filtre pour afficher tous les challenges ou seulement ceux du user

--- src/components/ChoicesChallenges.jsx
+++ src/components/ChoicesChallenges.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-function ChoicesChallenges() {
+function ChoicesChallenges({ setChoice }) {
   return (
     <ul className="items-center w-1/2 mx-auto text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
       <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
@@ -8,9 +8,10 @@ function ChoicesChallenges() {
           <input
             id="horizontal-list-radio-license"
             type="radio"
-            value=""
+            value="allChallenges"
             name="list-radio"
             className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+            onChange={(e) => setChoice(e.target.value)}
           />
           <label
             htmlFor="horizontal-list-radio-license"
@@ -26,9 +27,10 @@ function ChoicesChallenges() {
           <input
             id="horizontal-list-radio-passport"
             type="radio"
-            value=""
+            value="myChallenges"
             name="list-radio"
             className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+            onChange={(e) => setChoice(e.target.value)}
           />
           <label
             htmlFor="horizontal-list-radio-passport"
--- src/pages/Challenges.jsx
+++ src/pages/Challenges.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Header from "../components/Header"
 import ChoicesChallenges from "../components/ChoicesChallenges"
 import ChallengeCard from "../components/ChallengeCard"
@@ -6,17 +6,24 @@ import { useSelector } from "react-redux"
 
 function Challenges() {
   const { challenges } = useSelector((state) => state.challenge)
+  const [choice, setChoice] = useState("allChallenges")
   return (
     <>
       <Header />
       <h1 className="my-5 font-semibold text-blackdark:text-white text-center text-xl">
         Voici la liste de tous les challenges
       </h1>
-      <ChoicesChallenges />
+      <ChoicesChallenges setChoice={setChoice} />
       <section className="flex px-3 justify-center items-start gap-5 flex-wrap my-8">
-        {challenges.map((challenge) => {
-          return <ChallengeCard key={challenge.id} challenge={challenge} />
-        })}
+        {choice === "myChallenges"
+          ? challenges
+              .filter((challenge) => challenge.createurId === "1234")
+              .map((challenge) => (
+                <ChallengeCard key={challenge.id} challenge={challenge} />
+              ))
+          : challenges.map((challenge) => (
+              <ChallengeCard key={challenge.id} challenge={challenge} />
+            ))}
       </section>
     </>
   )
