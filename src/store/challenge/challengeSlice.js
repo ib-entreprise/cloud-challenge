21c0e5beee49ba24021e8afabd02d6b4e5997b80
Mehdi Benamar <mistahbe213@gmail.com>
2024-03-22 11:58:56 +0100

    add function to get one challenge

--- src/store/challenge/challengeSlice.js
+++ src/store/challenge/challengeSlice.js
@@ -4,14 +4,18 @@ const ChallengeSlice = createSlice({
   name: "challenge",
   initialState: {
     challenges: [],
+    challenge: {},
   },
   reducers: {
     setChallenges: (state, { payload }) => {
       state.challenges = payload
     },
+    getChallenge: (state, { payload }) => {
+      state.challenge = payload
+    },
   },
 })
 
-export const { setChallenges } = ChallengeSlice.actions
+export const { setChallenges, getChallenge } = ChallengeSlice.actions
 
 export default ChallengeSlice
