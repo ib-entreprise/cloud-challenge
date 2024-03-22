0a3607262c3a7fd30a3bdfd7e25d888959f09e47
Mehdi Benamar <mistahbe213@gmail.com>
2024-03-22 16:34:44 +0100

    manage color of links of the header

--- src/components/HeaderAuth.jsx
+++ src/components/HeaderAuth.jsx
@@ -1,7 +1,9 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { useLocation } from "react-router-dom"
 
 function HeaderAuth() {
+  const { pathname } = useLocation()
   return (
     <header className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -36,7 +38,9 @@ function HeaderAuth() {
             <li>
               <Link
                 to="/"
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+                className={`${
+                  pathname === "/" ? "text-blue-600" : "text-white"
+                } block py-2 px-3 rounded md:bg-transparent md:p-0  dark:bg-blue-600 md:dark:bg-transparent`}
                 aria-current="page"
               >
                 Connexion
@@ -46,7 +50,9 @@ function HeaderAuth() {
             <li>
               <Link
                 to="/register"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className={`${
+                  pathname === "/register" ? "text-blue-600" : "text-white"
+                } block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
               >
                 Inscription
               </Link>
